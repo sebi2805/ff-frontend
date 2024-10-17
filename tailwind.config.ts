7ec5f5485f39fdac7ed833b4cb69d02c12330545
Usama Balawi <balawiu@gmail.com>
2024-10-17 21:33:02 +0300

    added theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,36 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        purple: {
+          50: "#f4f0ff",
+          200: "#dccdff",
+          400: "#a872ff",
+          600: "#8712ff",
+          800: "#6900db",
+          950: "#330078",
+        },
+        blue: {
+          50: "#f0f1ff",
+          200: "#cdcfff",
+          400: "#7a72ff",
+          600: "#3b12ff",
+          800: "#2200db",
+          950: "#0c0078",
+        },
+        pink: {
+          50: "#fff2fc",
+          200: "#ffc7f1",
+          400: "#ff61ce",
+          600: "#f405bd",
+          800: "#a5037a",
+          950: "#5c0042",
+        },
       },
     },
   },
